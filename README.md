3fc36e0493464174269f56856128f27804feb00b
gracentruth <63465350+gracentruth@users.noreply.github.com>
2021-10-22 23:32:39 +0900

    practice FileReader

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # FileIO_practice
+
+##파일 입출력
+
+###1. 텍스트 파일 읽어오기 
+
+<ol> 1)FileReader
+
+- 형식 FileReader(File file)
+- 캐릭터 파일을 읽을 수 있는 기능 제공
+- 메소드 
+  + read(): 한글자씩 읽어서 int형 숫자를 return(char로 형 변환 시 문자)더 이상 글자가 없을시 -1 return 
+    
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,26 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Main {
+    public static void main(String[] args)throws IOException{
+        String path= Paths.get(".").toAbsolutePath().toString();
+        String filename=path+"/data.txt";
+
+        File file = new File(filename);
+
+        // 1. FileReader로 한 글자씩 읽어오기
+        FileReader reader=new FileReader(file);
+
+        int a=0;
+
+        while((a=reader.read())!=-1){
+            System.out.println((char)a);
+        }
+        //
+
+    }
+
 
 }
